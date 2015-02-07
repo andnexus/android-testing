b75ee2cbe312aa9eeeb3e28bba14d979e96eef7d
Benny <benjamin@andnexus.com>
2015-02-07 10:30:31 +0100

    Rise and Shine™ for UI Testing
    * Credits https://gist.github.com/JakeWharton/f50f3b4d87e57d8e96e9

--- app/src/debug/java/com/andnexus/android/tests/ActivityTest.java
+++ app/src/debug/java/com/andnexus/android/tests/ActivityTest.java
@@ -5,8 +5,11 @@ import com.andnexus.connect.Connect;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import android.app.Activity;
+import android.app.KeyguardManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.PowerManager;
 import android.test.ActivityInstrumentationTestCase2;
 
 import javax.inject.Inject;
@@ -14,6 +17,13 @@ import javax.inject.Singleton;
 
 import dagger.Provides;
 
+import static android.content.Context.KEYGUARD_SERVICE;
+import static android.content.Context.POWER_SERVICE;
+import static android.os.PowerManager.ACQUIRE_CAUSES_WAKEUP;
+import static android.os.PowerManager.FULL_WAKE_LOCK;
+import static android.os.PowerManager.ON_AFTER_RELEASE;
+import static android.view.WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED;
+import static com.andnexus.android.tests.MainActivity.OnCreateListener;
 import static org.mockito.Mockito.when;
 
 public abstract class ActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
@@ -64,5 +74,28 @@ public abstract class ActivityTest extends ActivityInstrumentationTestCase2<Main
         public Connect provideConnect() {
             return mConnect;
         }
+
+        @Provides
+        @Singleton
+        public OnCreateListener provideOnCreateListener() {
+            return new OnCreateListener() {
+                @Override
+                public void onCreate(Activity activity) {
+
+                    // http://developer.android.com/tools/testing/activity_testing.html#UnlockDevice
+                    KeyguardManager keyguardManager = (KeyguardManager) activity.getSystemService(KEYGUARD_SERVICE);
+                    KeyguardManager.KeyguardLock lock = keyguardManager.newKeyguardLock(MainActivity.class.getSimpleName());
+                    lock.disableKeyguard();
+
+                    // https://gist.github.com/JakeWharton/f50f3b4d87e57d8e96e9
+                    activity.getWindow().addFlags(FLAG_SHOW_WHEN_LOCKED);
+                    PowerManager power = (PowerManager) activity.getSystemService(POWER_SERVICE);
+                    PowerManager.WakeLock wakeLock = power.newWakeLock(FULL_WAKE_LOCK | ACQUIRE_CAUSES_WAKEUP | ON_AFTER_RELEASE, "wakeup!");
+                    wakeLock.acquire();
+                    wakeLock.release();
+                }
+            };
+        }
+
     }
 }
